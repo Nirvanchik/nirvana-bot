970e7bb717e137fa5537ba5790308d5a216e7b29
Dmitry Trofimovich <DimaTrofimovich@gmail.com>
2016-11-18 23:10:54 +0300

    Add commons-collections jar to scripts dependencies.

--- allru.cmd
+++ allru.cmd
@@ -1,6 +1,7 @@
 chcp 866
 java -classpath ^.\bin;^
 .\lib\commons-lang3-3.0.1.jar;^
+.\lib\commons-collections-3.2.1.jar;^
 .\lib\log4j-1.2.16.jar;^
 .\lib\jackson-core-2.0.6.jar;^
 .\lib\jackson-databind-2.0.6.jar;^
