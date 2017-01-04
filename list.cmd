d0b5911c00bbeed4aca72a6921275ec8249d6e9b
Dmitry Trofimovich <DimaTrofimovich@gmail.com>
2017-01-05 01:27:48 +0300

    Don't specify list of .jar dependencies in cmd. Prefer full /lib dir with jars instead. This is more friendly for updates of deps from time to time.

--- list.cmd
+++ list.cmd
@@ -1,1 +1,2 @@
-java -classpath .\bin;.\lib\commons-lang3-3.0.1.jar;.\lib\log4j-1.2.16.jar org.wikipedia.nirvana.ListTools %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath ^.\bin;.\lib\* ^
+org.wikipedia.nirvana.ListTools %1 %2 %3 %4 %5 %6 %7 %8 %9
