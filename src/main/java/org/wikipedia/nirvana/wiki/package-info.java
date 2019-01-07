ea4dfc8d6a905013d1e4193a9cc606ddb22635cc
Dmitry Trofimovich <DimaTrofimovich@gmail.com>
2019-01-07 22:38:24 +0300

    T #33 Create org.wikipedia.nirvana.wiki package.

---
+++ src/main/java/org/wikipedia/nirvana/wiki/package-info.java
@@ -0,0 +1,31 @@
+/**
+ *  @(#)package-info.java 7 January 2019 г.
+ *  Copyright © 2019 Dmitry Trofimovich (KIN, Nirvanchik, DimaTrofimovich@gmail.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * WARNING: This file may contain Russian characters.
+ * This file is encoded with UTF-8.
+ * */
+
+/**
+ * Wiki-related utilities.
+ *
+ * Extensions for Wiki.java framework.
+ * Wiki-sites related tools.
+ * Other utility code for manipulation with Wiki site or data from Wiki site.
+ */
+package org.wikipedia.nirvana.wiki;
