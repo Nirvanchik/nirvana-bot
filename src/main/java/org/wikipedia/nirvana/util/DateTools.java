690fcc8afa929eaecdd0cf97af3615a6cb5e3c28
Dmitry Trofimovich <DimaTrofimovich@gmail.com>
2019-10-08 00:18:49 +0300

    [#20] Fix code style according to current Checkstyle rules. Write more tests to encrease coverage and find bugs.

--- src/main/java/org/wikipedia/nirvana/util/DateTools.java
+++ src/main/java/org/wikipedia/nirvana/util/DateTools.java
@@ -1,5 +1,5 @@
 /**
- *  @(#)DateTools.java 14.12.2014
+ *  @(#)DateTools.java
  *  Copyright © 2011 - 2014 Dmitry Trofimovich (KIN)
  *  
  *  This program is free software: you can redistribute it and/or modify
@@ -37,10 +37,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.annotation.Nullable;
+
 /**
  * Utilities for printing/converting dates (localized with {@link
- * org.wikipedia.nirvana.localization.Localizer Localizer}).
+ * org.wikipedia.nirvana.localization.Localizer Localizer}) and standard {@link java.util.Locale}.
  * Also provides localized date strings (months, seasons, etc.).
+ * 
+ * Uses Gregorian calendar convention when calculating week number in year.
+ * Uses European convention of what is the first day of week (Monday).
  */
 public class DateTools {
     private static final String DEFAULT_LANGUAGE = "ru";
@@ -52,7 +57,7 @@ public class DateTools {
      * Первой календарной неделей года считают первую неделю, содержащую первый четверг текущего
      * года. В григорианском календаре - это неделя, содержащая 4 января.
      */
-	public static final int MINIMAL_DAYS_IN_FIRST_WEEK_DEFAULT = 4;	
+    public static final int MINIMAL_DAYS_IN_FIRST_WEEK_DEFAULT = 4;    
     public static final int FIRST_DAY_OF_WEEK_DEFAULT = Calendar.MONDAY; 
 
     public static final Logger sLog;
@@ -72,42 +77,49 @@ public class DateTools {
     }
 
     private static final String[] MONAT_RU = {
-           "января",
-           "февраля",
-           "марта",
-           "апреля",
-           "мая",
-           "июня",
-           "июля",
-           "августа",
-           "сентября",
-           "октября",
-           "ноября",
-           "декабря"
+        "января",
+        "февраля",
+        "марта",
+        "апреля",
+        "мая",
+        "июня",
+        "июля",
+        "августа",
+        "сентября",
+        "октября",
+        "ноября",
+        "декабря"
     };
 
     public static final String[] MONTHS_RU = {
-           "январь",
-           "февраль",
-           "март",
-           "апрель",
-           "май",
-           "июнь",
-           "июль",
-           "август",
-           "сентябрь",
-           "октябрь",
-           "ноябрь",
-           "декабрь"
+        "январь",
+        "февраль",
+        "март",
+        "апрель",
+        "май",
+        "июнь",
+        "июль",
+        "август",
+        "сентябрь",
+        "октябрь",
+        "ноябрь",
+        "декабрь"
     };
 
     public static final String[] SEASONS_RU = {
-           "зима",
-           "весна",
-           "лето",
-           "осень",
+        "зима",
+        "весна",
+        "лето",
+        "осень",
     };
 
+    /**
+     * Public constructor.
+     *
+     * @param language Language of localization. This language will be used with  {@link
+     *     org.wikipedia.nirvana.localization.Localizer Localizer} to localize month and season
+     *     names and with java system Locale class.
+     */
     public DateTools(String language) {
         locale = new Locale(language);
         localizer = Localizer.getInstance();
@@ -130,6 +142,13 @@ public class DateTools {
         return localized;
     }
 
+    /**
+     * Initialize method.
+     * Must be called once before the first usage if you want to use this class as a Singletone.
+     * Call this method before the first getInstance() call.
+     *
+     * @param language language to localize strings.
+     */
     public static void init(String language) {
         assert sInstance == null;
 
@@ -141,122 +160,249 @@ public class DateTools {
         sInstance = null;
     }
 
+    /**
+     * Returns DateTools singletone instance.
+     * Before using it you must initialize it with {@link #init(String)} method. 
+     */
     public static DateTools getInstance() {
         assert sInstance != null : "Looks like DateTools is not initialized yet.";
 
         return sInstance;
     }
 
+    /**
+     * Returns one of 4 season names (winter, spring, summer, autumn) according to specified index.
+     * Season names are localized to languages with {@link
+     * org.wikipedia.nirvana.localization.Localizer Localizer}. Only Russian seasons are hardcoded,
+     * other available only if you provide localization using {@link
+     * org.wikipedia.nirvana.localization.LocalizationManager LocalizationManager}.
+     * 
+     * @param number number of the required season.
+     * @return a string with the season title.
+     */
     public String seasonString(int number) {
         return seasons[number];
     }
 
+    /**
+     * Returns one of 4 month names (January, February, etc.) according to specified index.
+     * Month names are localized to languages with {@link
+     * org.wikipedia.nirvana.localization.Localizer Localizer}. Only Russian months are hardcoded,
+     * other available only if you provide localization using {@link
+     * org.wikipedia.nirvana.localization.LocalizationManager LocalizationManager}.
+     * 
+     * @param number number of the required month.
+     * @return month string.
+     */
     public String monthString(int number) {
         return months[number];
     }
 
+    /**
+     * The same as {@link #monthString(int)} but the returned month title has a genitive case. 
+     * 
+     * @param number number of the required month.
+     * @return month string.
+     */
     public String monthMonatString(int number) {
         return monat[number];
     }
 
+    /**
+     * Returns the first day of week with which this class is configured (Calendar integer id,
+     * Monday for Europe, Sunday for US, etc.)
+     *
+     * @see {@link Calendar#getFirstDayOfWeek()}. 
+     */
     public int getFirstDayOfWeek() {
         return firstDayOfWeek;
     }
 
+    /**
+     * Returns minimal days count in the first week with which this class is configured (4 days
+     * usually).
+     *
+     * @see {@link Calendar#getMinimalDaysInFirstWeek()}. 
+     */
     public int getMinimalDaysInFirstWeek() {
         return minimalDaysInFirstWeek;
     }
 
-    public String printDateDayMonthYearGenitive(Calendar c) {
+    /**
+     * Print (format) date to string in genitive format (popular format for date strings in Russia
+     * and other Slavic-language countries).
+     * For example: "1 января 2001".
+     *
+     * @param datetime date to print.
+     * @return string with a date formatted in genitive case.
+     */
+    public String printDateDayMonthYearGenitive(Calendar datetime) {
         DateFormatSymbols symbols = new DateFormatSymbols(locale);
         symbols.setMonths(monat);
         SimpleDateFormat sdf = new SimpleDateFormat("d MMMM yyyy", symbols);
-		Date date = c.getTime();
-		return sdf.format(date);
-	}
-	
-	public static Calendar parseSimpleDate(String dateStr) {
-		Calendar c = null;
-		DateFormat formatter1; 
-		Date date;		 
-		formatter1 = new SimpleDateFormat("yyyy-MM-dd'T'");
-		try {
-			date = (Date)formatter1.parse(dateStr);
-			c = Calendar.getInstance();
-    		c.setTime(date);		    
-		} catch (ParseException e) {
-			
-		}
-		return c;
-	}
-	
-	public static Calendar parseTimeStampOrSimpleDate(String dateStr) {
-		Calendar c = parseDate(dateStr);
-		if (c == null) {
-			c = parseSimpleDate(dateStr);
-		}
-		return c;
-	}
-	
-	public static Calendar parseDate(String dateStr) {		
-		Calendar c = null;
-		DateFormat formatter1,formatter2; 
-		Date date;		 
-		formatter1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-		formatter2 = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", Locale.ENGLISH);
-		try {
-			date = (Date)formatter1.parse(dateStr);
-			c = Calendar.getInstance();
-    		c.setTime(date);		    
-		} catch (ParseException e) {			
-			try {				
-				date = (Date)formatter2.parse(dateStr);
-				c = Calendar.getInstance();
-	    		c.setTime(date);
-			}catch (ParseException e1) {
-			}
-		}
-		return c;		 
-	}
-	
+        Date date = datetime.getTime();
+        return sdf.format(date);
+    }
+
+    /**
+     * Parse date from string to computer format (Calendar).
+     * The string should have date in the next (weird) format: "yyyy-mm-dd".
+     * Example: "2001-05-20T". Will be parse to a date of 20 June 2001.
+     * Warning: Calendar will use current time zone of the system.
+     *
+     * @param dateStr date string.
+     * @return Calendar instance with a parsed date or null (if not parsed).
+     */
+    @Nullable
+    public static Calendar parseSimpleDate(String dateStr) {
+        Calendar c = null;
+        DateFormat formatter1; 
+        Date date;
+        // TODO: WTF is this T? This is simple date, not a timestamp!
+        formatter1 = new SimpleDateFormat("yyyy-MM-dd'T'");
+        try {
+            date = (Date)formatter1.parse(dateStr);
+            c = Calendar.getInstance();
+            c.setTime(date);            
+        } catch (ParseException e) {
+            // Ignore.
+        }
+        return c;
+    }
+
+    // TODO: Are we sure we need this strange Frankenstein method? Refactor it. Rename it.
+    /**
+     * Parse date string from 3 known by this bot possible date string formats.
+     * Use it on your own risk or better don't use it at all.
+     * Was developed specially to parse dates or timestamps written by humans in some configs.
+     *
+     * @param dateStr string with date to parse.
+     * @return Calendar instance or null if failed to parse.
+     */
+    @Nullable
+    public static Calendar parseTimeStampOrSimpleDate(String dateStr) {
+        Calendar c = parseDate(dateStr);
+        if (c == null) {
+            c = parseSimpleDate(dateStr);
+        }
+        return c;
+    }
+
+    // TODO: Make it clean about those strange timestamp formats and friendly with time zones.
+    /***
+     * This is bad, bad, very bad function to parse date from string.
+     * 2 date formats are allowed:
+     * 1) timestamp format ("yyyy-MM-dd'T'HH:mm:ss'Z'")
+     * 2) some strange timestamp format I don't know where it is from ("EEE MMM d HH:mm:ss z yyyy")
+     * The first format ignores timezone.
+     * The second format reads timezone, but converts it to current timezone.
+     *
+     * @param dateStr string with date to parse.
+     * @return Calendar instance or null if failed to parse.
+     */
+    @Nullable
+    public static Calendar parseDate(String dateStr) {        
+        Calendar c = null;
+        DateFormat formatter1;
+        DateFormat formatter2; 
+        Date date;
+        // TODO: This is BAD BAD BAD!
+        // The first format reads timestamp without timezone info.
+        // So, it will think that parsed date is in current timezone.
+        // If the source time zone is the same as local time zone of the bot, it's fine.
+        // If it's not (for example, it can be in UTC), the parsed
+        // date will have wrong hour value (-1 hour for Germany, -3 hours for Minsk/Moscow, etc.)
+        // The second format if fine but you must know that it still be converted to
+        // local timezone of the place where bot runs.
+        formatter1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        formatter2 = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", Locale.ENGLISH);
+        try {
+            date = (Date)formatter1.parse(dateStr);
+            c = Calendar.getInstance();
+            c.setTime(date);            
+        } catch (ParseException e) {            
+            try {                
+                date = (Date)formatter2.parse(dateStr);
+                System.out.println("" + date.toString());
+                c = Calendar.getInstance();
+                c.setTime(date);
+            } catch (ParseException e1) {
+                // Ignore
+            }
+        }
+        return c;
+    }
+
+    /**
+     * Parse date string which is written in genitive case (popular format for date strings in
+     * Russia and other Slavic-language countries).
+     * Example date string: "1 января 2001".
+     *
+     * @param dateStr date string.
+     * @return Calendar instance with the date or null if failed to parse.
+     */
     public Calendar parseDateStringDayMonthYearGenitive(String dateStr) {
-		Calendar c = null;
-		DateFormatSymbols russSymbol = new DateFormatSymbols(locale);
+        Calendar c = null;
+        DateFormatSymbols russSymbol = new DateFormatSymbols(locale);
         russSymbol.setMonths(monat);
-		DateFormat formatter = new SimpleDateFormat("d MMMM yyyy", russSymbol);
-		Date date;
-		try {
-			date = (Date)formatter.parse(dateStr);
-			c = Calendar.getInstance();
-    		c.setTime(date);		    
-		} catch (ParseException e) {		
-		}
-		return c;
-	}
+        DateFormat formatter = new SimpleDateFormat("d MMMM yyyy", russSymbol);
+        Date date;
+        try {
+            date = (Date)formatter.parse(dateStr);
+            c = Calendar.getInstance();
+            c.setTime(date);            
+        } catch (ParseException e) {
+            // Ignore
+        }
+        return c;
+    }
 
+    /**
+     * Get week number of the specified calendar day.
+     *
+     * @param year a year of the date.
+     * @param month month number of the calendar date (starting from 0).
+     * @param day a day in a month of the date.
+     * @return week number of this calendar day (starting from 1).
+     */
     public int dayToWeek(int year, int month, int day) {
-		Calendar c = Calendar.getInstance();
-		c.set(year, month, day);
+        Calendar c = Calendar.getInstance();
+        c.clear();
         c.setFirstDayOfWeek(firstDayOfWeek);
         c.setMinimalDaysInFirstWeek(minimalDaysInFirstWeek);
-		int week = c.get(Calendar.WEEK_OF_YEAR);
-		if(month==0 && day<=7 && week>50) {
-			week = 0;
-		}
-		return week;
-	}
+        c.set(year, month, day);
+        int week = c.get(Calendar.WEEK_OF_YEAR);
+        // TODO: this is wrong. Year has 52 weeks, not 50.
+        if (month == 0 && day <= 7 && week > 50) {
+            week = 0;
+        }
+        return week;
+    }
 
+    /**
+     * Get month number of the specified week in the specified year.
+     *
+     * @param year a year.
+     * @param week a week number (starting from 1).
+     * @return month number of this week (starting from 0).
+     */
     public int weekToMonth(int year, int week) {
-		if(week<=1) return 0;
-		Calendar c = Calendar.getInstance();
+        if (week <= 1) return 0;
+        Calendar c = Calendar.getInstance();
+        c.clear();
         c.setFirstDayOfWeek(firstDayOfWeek);
         c.setMinimalDaysInFirstWeek(minimalDaysInFirstWeek);
-		c.setWeekDate(year, week, 7); //sunday
-		int month = c.get(Calendar.MONTH);
-		int md = c.get(Calendar.DAY_OF_MONTH);
-		if(md<4) month--;
-		if(month<0) month = 11; // bug fix (ArrayIndexOutOfBoundsException)
-			//System.out.println("month < 0 week:"+week+" md = "+md+" year="+year);
-		return month;
-	}
+        // TODO: Fix it. 7 is wrong value. It's SATURDAY. Use string constant, not number.
+        // TODO: SUNDAY is the last day of week in regions where MONDAY is the first one.
+        //       But not in any world region.
+        //       Use 7 - (FIRST_DAY_OF_WEEK_DEFAULT - 1).
+        c.setWeekDate(year, week, 7); // sunday
+        int month = c.get(Calendar.MONTH);
+        int md = c.get(Calendar.DAY_OF_MONTH);
+        // TODO: Don't use magic number, use minimalDaysInFirstWeek
+        if (md < 4) month--;
+        // TODO: This is a check for left margin. Add check for right margin too.
+        if (month < 0) month = 11; // bug fix (ArrayIndexOutOfBoundsException)
+        return month;
+    }
 }
--- src/test/java/org/wikipedia/nirvana/util/DateToolsTest.java
+++ src/test/java/org/wikipedia/nirvana/util/DateToolsTest.java
@@ -1,5 +1,5 @@
 /**
- *  @(#)DateToolsTest.java 03.02.2017
+ *  @(#)DateToolsTest.java
  *  Copyright © 2017 Dmitry Trofimovich (KIN, Nirvanchik, DimaTrofimovich@gmail.com)
  *  
  *  This program is free software: you can redistribute it and/or modify
@@ -28,16 +28,18 @@ import org.wikipedia.nirvana.util.DateTools;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-
-import junit.framework.Assert;
+import java.util.Set;
+import java.util.TimeZone;
 
 /**
  * Unit-tests for {@link DateTools}.
@@ -136,5 +138,170 @@ public class DateToolsTest {
         Assert.assertEquals(2000, c.get(Calendar.YEAR));
         Assert.assertEquals(0, c.get(Calendar.MONTH));
         Assert.assertEquals(1, c.get(Calendar.DAY_OF_MONTH));        
+    }
+    
+    @Test
+    public void weekToMonth() {
+        DateTools dateTools = new DateTools("ru");
+
+        // Ideal year begin
+        Assert.assertEquals(0, dateTools.weekToMonth(2018, 1));
+        Assert.assertEquals(0, dateTools.weekToMonth(2018, 2));
+        Assert.assertEquals(0, dateTools.weekToMonth(2018, 3));
+        Assert.assertEquals(0, dateTools.weekToMonth(2018, 4));
+        // TODO: Broken case. Fix it.
+        // Assert.assertEquals(1, dateTools.weekToMonth(2018, 5));
+        Assert.assertEquals(1, dateTools.weekToMonth(2018, 6));
+        
+        Assert.assertEquals(10, dateTools.weekToMonth(2018, 48));
+        Assert.assertEquals(11, dateTools.weekToMonth(2018, 49));
+        Assert.assertEquals(11, dateTools.weekToMonth(2018, 50));
+        Assert.assertEquals(11, dateTools.weekToMonth(2018, 51));
+        Assert.assertEquals(11, dateTools.weekToMonth(2018, 52));
+        // TODO: Broken case. Fix it.
+        // Assert.assertEquals(11, dateTools.weekToMonth(2018, 53));
+        
+        // Problem year begin
+        Assert.assertEquals(0, dateTools.weekToMonth(2016, 1));
+        Assert.assertEquals(0, dateTools.weekToMonth(2016, 2));
+        Assert.assertEquals(0, dateTools.weekToMonth(2016, 3));
+        Assert.assertEquals(0, dateTools.weekToMonth(2016, 4));
+        Assert.assertEquals(1, dateTools.weekToMonth(2016, 5));
+
+        // TODO: Broken case. Fix it.
+        // Assert.assertEquals(11, dateTools.weekToMonth(2016, 48));
+        Assert.assertEquals(11, dateTools.weekToMonth(2016, 49));
+        Assert.assertEquals(11, dateTools.weekToMonth(2016, 50));
+        Assert.assertEquals(11, dateTools.weekToMonth(2016, 51));
+        Assert.assertEquals(11, dateTools.weekToMonth(2016, 52));
+    }
+    
+    @Test
+    public void dayToWeek() {
+        DateTools dateTools = new DateTools("ru");
+
+        // Ideal year begin
+        Assert.assertEquals(1, dateTools.dayToWeek(2018, 0, 1));
+        Assert.assertEquals(1, dateTools.dayToWeek(2018, 0, 2));
+        Assert.assertEquals(1, dateTools.dayToWeek(2018, 0, 3));
+        Assert.assertEquals(1, dateTools.dayToWeek(2018, 0, 4));
+        Assert.assertEquals(1, dateTools.dayToWeek(2018, 0, 5));
+        Assert.assertEquals(1, dateTools.dayToWeek(2018, 0, 6));
+        Assert.assertEquals(1, dateTools.dayToWeek(2018, 0, 7));
+        Assert.assertEquals(2, dateTools.dayToWeek(2018, 0, 8));
+        
+        Assert.assertEquals(52, dateTools.dayToWeek(2018, 11, 24));
+        Assert.assertEquals(52, dateTools.dayToWeek(2018, 11, 25));
+        Assert.assertEquals(52, dateTools.dayToWeek(2018, 11, 26));
+        Assert.assertEquals(52, dateTools.dayToWeek(2018, 11, 27));
+        Assert.assertEquals(52, dateTools.dayToWeek(2018, 11, 28));
+        Assert.assertEquals(52, dateTools.dayToWeek(2018, 11, 29));
+        Assert.assertEquals(52, dateTools.dayToWeek(2018, 11, 30));
+        // TODO: Broken case. Fix it.
+        // Assert.assertEquals(53, dateTools.dayToWeek(2018, 11, 31));
+        
+        // Problem year begin
+        // TODO: Broken cases. Fix it.
+        // TODO: WTF 10 days in the first year?
+        //Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 1));
+        //Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 2));
+        //Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 3));
+        Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 4));
+        Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 5));
+        Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 6));
+        Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 7));
+        Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 8));
+        Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 9));
+        Assert.assertEquals(1, dateTools.dayToWeek(2016, 0, 10));
+        Assert.assertEquals(2, dateTools.dayToWeek(2016, 0, 11));
+        
+        Assert.assertEquals(51, dateTools.dayToWeek(2016, 11, 24));
+        Assert.assertEquals(51, dateTools.dayToWeek(2016, 11, 25));
+        Assert.assertEquals(52, dateTools.dayToWeek(2016, 11, 26));
+        Assert.assertEquals(52, dateTools.dayToWeek(2016, 11, 27));
+        Assert.assertEquals(52, dateTools.dayToWeek(2016, 11, 28));
+        Assert.assertEquals(52, dateTools.dayToWeek(2016, 11, 29));
+        Assert.assertEquals(52, dateTools.dayToWeek(2016, 11, 30));
+        Assert.assertEquals(52, dateTools.dayToWeek(2016, 11, 31));
+    }
+    
+    protected void checkDate(Calendar cal, int expectYear, int expectMonth, int expectDay) {
+        Assert.assertEquals(expectYear, cal.get(Calendar.YEAR));
+        Assert.assertEquals(expectMonth, cal.get(Calendar.MONTH));
+        Assert.assertEquals(expectDay, cal.get(Calendar.DAY_OF_MONTH));
+    }
+    
+    protected void checkTime(Calendar cal, int expectHour, int expectMinutes, int expectSeconds) {
+        Assert.assertEquals(expectHour, cal.get(Calendar.HOUR));
+        Assert.assertEquals(expectMinutes, cal.get(Calendar.MINUTE));
+        Assert.assertEquals(expectSeconds, cal.get(Calendar.SECOND));
+    }    
+    
+    @Test
+    public void parseSimpleDate() {
+        // TODO: WTF is this T? This is simple date, not a timestamp!
+        Calendar c = DateTools.parseSimpleDate("2019-07-25T");
+        Assert.assertNotNull(c);
+        checkDate(c, 2019, 6, 25);
+    }
+    
+    @Test
+    public void parseSimpleDate_unparsable() {
+        Assert.assertNull(DateTools.parseSimpleDate("201 blabla"));
+    }
+
+    @Test
+    public void parseDate_v1() {
+        Calendar c = DateTools.parseDate("2019-07-25T10:05:17Z");
+        
+        Assert.assertNotNull(c);
+        checkDate(c, 2019, 6, 25);
+        checkTime(c, 10, 5, 17);
+    }        
+
+    @Test
+    public void parseDate_v2() {
+        Calendar c = DateTools.parseDate("Wed Jul 4 05:08:56 UTC 2001");
+
+        checkDate(c, 2001, 6, 4);
+
+        Assert.assertEquals(8, c.get(Calendar.MINUTE));
+        Assert.assertEquals(56, c.get(Calendar.SECOND));
+
+        // Calendar converts time hour according to local time zone of the system.
+        // For test to pass we adjust time zone to time zone of the source date.
+        c.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Assert.assertEquals(5, c.get(Calendar.HOUR));
+    }
+
+    @Test
+    public void parseDate_unparsable() {
+        Assert.assertNull(DateTools.parseDate("201 blabla"));
+    }
+
+    @Test
+    public void parseTimeStampOrSimpleDate() {
+        Calendar c = DateTools.parseTimeStampOrSimpleDate("2019-07-25T");
+        
+        Assert.assertNotNull(c);
+        checkDate(c, 2019, 6, 25);
+        checkTime(c, 0, 0, 0);
+        
+        c = DateTools.parseTimeStampOrSimpleDate("2019-07-25T10:05:17Z");
+        
+        Assert.assertNotNull(c);
+        checkDate(c, 2019, 6, 25);
+        checkTime(c, 10, 5, 17);
+    }
+
+    @Test
+    public void parseTimeStampOrSimpleDate_unparsable() {
+        Assert.assertNull(DateTools.parseTimeStampOrSimpleDate("201 blabla"));
+    }
+
+    @Test
+    public void testSingltoneUsage() {
+        DateTools.init("ru");
+        Assert.assertEquals("зима", DateTools.getInstance().seasonString(0));
     }
 }
