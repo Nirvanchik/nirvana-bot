Nirvanchik/nirvana-bot

commit c4411dc50db449f8126787d46e90a4c59bca1cd8

[#80] Add workaround against spontaneous dislogin. Mediawiki API tells that when user is not logged in the API returns empty CSRF token (with a weird value of "+\"). See https://www.mediawiki.org/wiki/API:Edit#Additional_notes.

src/main/java/org/wikipedia/nirvana/wiki/NirvanaWiki.java
@@ -857,6 +857,13 @@ public class NirvanaWiki extends Wiki {
         super.login(username, tempPassword);
     }
 
+    @Override
+    public synchronized void logout() {
+        this.username = null;
+        this.password = null;
+        super.logout();
+    }
+
     /**
      * Relogin (log out and immediately log in).
      * Call it when there was no interaction with Wiki for too long.
@@ -864,6 +871,17 @@ public class NirvanaWiki extends Wiki {
     public synchronized void relogin() throws FailedLoginException, IOException {
         this.logout();
         this.login(this.username, this.password);
+    }
+
+    @Override
+    public String getToken(String type) throws IOException {
+        String token = super.getToken(type);
+        if (username != null && type.equals("csrf")) {
+            if (token.isEmpty() || token.equals("+\\")) {
+                throw new RuntimeException("Unexpected Dislogin");
+            }
+        }
+        return token;
     }
 
     public void dumpCookies() {

src/main/java/org/wikipedia/nirvana/nirvanabot/NirvanaBot.java
@@ -1034,7 +1034,32 @@ public class NirvanaBot extends BasicBot{
     				log.fatal(e.toString());
     				fatalProblem = true;
     				break;
-    				//e.printStackTrace();
+                } catch (RuntimeException e) {
+                    log.error(e.toString());
+                    if (tryNumber < tryCount) {
+                        if (e.getMessage().equals("Unexpected Dislogin")) {
+                            try {
+                                log.info("Dislogin detected. RETRY AGAIN after relogin...");
+                                wiki.relogin();
+                                retry = true;
+                            } catch (FailedLoginException | IOException e1) {
+                                log.fatal(e.toString());
+                                reporter.portalError();
+                                reportItem.error();
+                                mayBeSomeServiceProblem = true;
+                                serviceManager.getMainWikiService().setNeedsRelogin(true);
+                                log.error("OOOOPS!!!", e);  // print stack trace
+                            }
+                        } else {
+                            log.info("RETRY AGAIN");
+                            retry = true;    
+                        }
+                    } else {
+                        reporter.portalError();
+                        reportItem.error();
+                        //e.printStackTrace();
+                        log.error("OOOOPS!!!", e); // print stack trace
+                    }
     			} catch (Exception e) {
     				log.error(e.toString()); 
                     if (tryNumber < tryCount) {

commit 40216023940136a65084d9b1b519c3d5baeaa84c

[#80] Fix relogin, that was broken after c4411dc50db449f8126787d46e90a4c59bca1cd8. Add test for dislogin handling.

src/main/java/org/wikipedia/nirvana/wiki/NirvanaWiki.java
@@ -79,6 +79,7 @@ public class NirvanaWiki extends Wiki {
     private String dumpFolder = null;
     protected boolean logDomain = false;
 
+    private boolean loggedIn = false;
     private String username;
     private char[] password;
     
@@ -855,13 +856,13 @@ public class NirvanaWiki extends Wiki {
         char[] tempPassword = Arrays.copyOf(password, password.length);
         // This will erase data in tempPassword - that is why not password
         super.login(username, tempPassword);
+        loggedIn = true;
     }
 
     @Override
     public synchronized void logout() {
-        this.username = null;
-        this.password = null;
         super.logout();
+        this.loggedIn = false;
     }
 
     /**
@@ -876,7 +877,7 @@ public class NirvanaWiki extends Wiki {
     @Override
     public String getToken(String type) throws IOException {
         String token = super.getToken(type);
-        if (username != null && type.equals("csrf")) {
+        if (loggedIn && type.equals("csrf")) {
             if (token.isEmpty() || token.equals("+\\")) {
                 throw new RuntimeException("Unexpected Dislogin");
             }
@@ -884,6 +885,9 @@ public class NirvanaWiki extends Wiki {
         return token;
     }
 
+    /**
+     * Print cookies to log. Used for critical problems (dislogin) debug.
+     */
     public void dumpCookies() {
         List<HttpCookie> cookieList = cookies.getCookieStore().getCookies();
         log.info("Show cookies for wiki: {}", this.domain);

src/test/java/org/wikipedia/nirvana/wiki/MockNirvanaWiki.java
@@ -64,6 +64,8 @@ public class MockNirvanaWiki extends NirvanaWiki {
 
     private LinkedList<String> fetchQueue = new LinkedList<>();
 
+    private String token;
+
     private User user;
 
     private List<EditInfo> edits = new ArrayList<>();
@@ -507,6 +509,15 @@ public class MockNirvanaWiki extends NirvanaWiki {
         log.debug("[MOCK] getRevisionText: {}", revId);
         Assert.assertTrue(ASSERT_MSG, revTextMap.containsKey(revId));
         return revTextMap.get(revId);
+    }
+
+    public void mockToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String getToken(String type) throws IOException {
+        return token;
     }
 
     public void debug() {

src/test/java/org/wikipedia/nirvana/wiki/NirvanaWikiTest.java
@@ -33,6 +33,7 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -278,9 +279,9 @@ public class NirvanaWikiTest {
     public void loginAndRelogin() throws Exception {
         MockNirvanaWiki wiki = new MockNirvanaWiki("test.xyz");
         wiki.allowLogin(true);
-        wiki.mockFetchSequential("logintoken=\"ABC1\"");
+        wiki.mockToken("ABC1");
         wiki.mockFetchSequential("result=\"Success\" lgusername=\"Bob\"");
-        wiki.mockFetchSequential("logintoken=\"ABC2\"");
+        wiki.mockToken("ABC2");
         wiki.mockFetchSequential("result=\"Success\" lgusername=\"Bob\"");
         String [] userrights = {"god"};
         Map<String, Object> userinfo = new HashMap<>();
@@ -296,6 +297,38 @@ public class NirvanaWikiTest {
         Assert.assertEquals(user1.getUsername(), "Bob");
 
         Assert.assertTrue(user1 != user2);
+    }
+
+    @Ignore("Requires to mock getPageInfo()")
+    @Test
+    public void doNotEditWhenDislogin() throws Exception {
+        MockNirvanaWiki wiki = new MockNirvanaWiki("test.xyz");
+        wiki.allowLogin(true);
+        wiki.allowEdits(true);
+        wiki.mockToken("ABC1");
+        wiki.mockFetchSequential("result=\"Success\" lgusername=\"Bob\"");
+
+        String [] userrights = {"god"};
+        Map<String, Object> userinfo = new HashMap<>();
+        userinfo.put("rights", userrights);
+        wiki.mockUserInfo("Bob", userinfo);
+
+        wiki.login("Bob", "12345");
+        User user1 = wiki.getCurrentUser();
+        Assert.assertEquals(user1.getUsername(), "Bob");
+        
+        wiki.mockToken("+\\");
+
+        try {
+            wiki.edit("Some Page", "empty", "fake edit for test");
+        } catch (RuntimeException e) {
+            if (e.getMessage().equals("Unexpected Dislogin")) {
+                return;
+            }
+            throw e;
+        }
+        
+        Assert.fail("edit wat not prevented after dislogin");
     }
 
     @Test