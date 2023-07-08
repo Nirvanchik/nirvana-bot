3ee8c2f29273a3d5ec765f40b182b1ca0fd30a4c
Dmitry Trofimovich <DimaTrofimovich@gmail.com>
2023-07-08 23:06:33 +0300

    [#111] Reformat StatisticsYear according to codestyle.

--- src/main/java/org/wikipedia/nirvana/statistics/StatisticsYear.java
+++ src/main/java/org/wikipedia/nirvana/statistics/StatisticsYear.java
@@ -1,6 +1,6 @@
 /**
- *  @(#)StatisticsYear.java 20/10/2012
- *  Copyright © 2012 Dmitry Trofimovich (KIN)
+ *  @(#)StatisticsYear.java
+ *  Copyright © 2023 Dmitry Trofimovich (KIN, Nirvanchik, DimaTrofimovich@gmail.com)
  *    
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@
  * WARNING: This file may contain Russian characters.
  * This file is encoded with UTF-8.
  * */
+
 package org.wikipedia.nirvana.statistics;
 
 import org.wikipedia.nirvana.nirvanabot.NirvanaBot;
@@ -36,155 +37,157 @@ import java.util.Map;
 import javax.management.BadAttributeValueExpException;
 
 /**
- * @author kin
+ * Statistics table where each row shows statistics per year.
  *
  */
 public class StatisticsYear extends Statistics {
-	String reportTemplate = "";
-	int startYear = 2008;
-	/**
-	 * @param type
-	 * @throws BadAttributeValueExpException
-     * @throws IOException 
-	 */
+    String reportTemplate = "";
+    int startYear = 2008;
+
+    /**
+     * Constructs class instance to generate statistics.
+     */
     public StatisticsYear(NirvanaWiki wiki, String cacheDir, String type, SystemTime systemTime)
             throws BadAttributeValueExpException, IOException {
         super(wiki, cacheDir, type, systemTime);
-		startYear = 2008;
-	}
-	
-	public void setOptions(Map<String,String> options) {
-		super.setOptions(options);
+        startYear = 2008;
+    }
+
+    @Override
+    public void setOptions(Map<String,String> options) {
+        super.setOptions(options);
         String key = "отчет";
-		if(options.containsKey(key) && !options.get(key).isEmpty()) {
-			this.reportTemplate = options.get(key);
-		}
+        if (options.containsKey(key) && !options.get(key).isEmpty()) {
+            this.reportTemplate = options.get(key);
+        }
         key = "первый год";
-		if(options.containsKey(key) && !options.get(key).isEmpty()) {
-			try {
-				this.startYear = Integer.parseInt(options.get(key));
-			} catch(NumberFormatException e) {
+        if (options.containsKey(key) && !options.get(key).isEmpty()) {
+            try {
+                this.startYear = Integer.parseInt(options.get(key));
+            } catch (NumberFormatException e) {
                 log.warn(String.format(NirvanaBot.ERROR_PARSE_INTEGER_FORMAT_STRING_EN, key,
                         options.get(key)));
-			}					
-		}
-	}
+            }
+        }
+    }
 
-	public void put(ArchiveDatabase2 db) throws IllegalStateException {
-		Calendar c = Calendar.getInstance();
-		// 1) find out period of the first week
-		ListIterator<ArchiveItem> it = null;
-		it = db.getIterator();
-		HashMap<String,Integer> userstat = new HashMap<String,Integer>(100);
-		//HashMap<Integer,Integer> articlestat = new HashMap<Integer,Integer>(7);
-		int articlestat[] = new int[366];
-		Arrays.fill(articlestat, 0);
-		
-		int total = 0;
-		int y = 0;
-		int day = 0;
-		ArchiveItem item = null;
-		while(it.hasNext()) {
-			item = it.next();			
-			y = item.year;		
-			if(y>=this.startYear) { // skip articles before the required year
-				userstat.put(item.user, 1);
-				c.set(item.year, item.month, item.day);
-				day = c.get(Calendar.DAY_OF_YEAR);
-				articlestat[day-1] = 1;
-				total = 1;
-				break;
-			}
-		}
-		
-		while(it.hasNext()) {
-			item = it.next();
-			if(item.year!=y) {
-				// calculate stat and add to list 
-				this.addStat(y, total, articlestat, userstat);
-				y = item.year;
-				// zero stat
-				total = 0;
-				Arrays.fill(articlestat, 0);
-				userstat.clear();
-			} 
-			// add item to current stat			
-			Integer n = userstat.get(item.user);
-			if(n==null) userstat.put(item.user, 1);
-			else userstat.put(item.user, n+1);	
-			c.set(item.year, item.month, item.day);
-			day = c.get(Calendar.DAY_OF_YEAR);
-			articlestat[day-1]++;
-			total++;			
-		}
-		// last stat
-		if(item!=null)
-			this.addStat(y, total, articlestat, userstat);
-		
-		calcTotal();
-	}
-	
-	void addStat(int year, int total, int articlestat[],Map<String,Integer> userstat) {
-		log.debug("adding item: year="+year+" total:"+total);
-		StatItem stat = new StatItem();
-		stat.number = items.size()+1;	
-		stat.month = 0;
-		stat.year = year;
-		stat.articles = total;
-		
-		int min = 1000000;
-		int max = 0;
-		int sum = 0;
-		Calendar c = Calendar.getInstance();
-		c.set(year, 1, 1);
-		int mdays = c.getActualMaximum(Calendar.DAY_OF_YEAR);
-		for(int i=0;i<mdays;i++) {
-			int n =articlestat[i];
-			sum +=n;
-			if(n<min) min = n;
-			if(n>max) max = n;
-		}
-		stat.articlesDayMid = ((float)sum/(float)mdays);
-		stat.articlesDayMax = max;
-		stat.articlesDayMin = min;
-		String user = "nobody";
-		int userc = -1;
-		for (Map.Entry<String, Integer> entry : userstat.entrySet()) {
-			if(entry.getValue()>userc) {
-				user = entry.getKey();
-				userc = entry.getValue();
-			}
-		}			
-		stat.user = user;
-		stat.userArticles = userc;
-		if(!items.isEmpty()) {
-			StatItem prev = this.items.get(items.size()-1);
-			stat.progress = stat.articles - prev.articles;
-			//stat.progress = (int) ((prev.articles - stat.articles)/(prev.articles==0?1:(prev.articles*0.1)));
-		}
-		this.items.add(stat);
-		addUserStat(userstat);
-	}
-	
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		sb.append(customHeader);
-		sb.append(header);
-		sb.append(DELIMETER);
-		for(StatItem item:items) {
-			String str = item.toString();
-			str = str.replace("%(отчет)", 
-					this.reportTemplate.replace("%(год)", String.valueOf(item.year)));
-			sb.append(str);
-			sb.append(DELIMETER);
-		}		
-		if(this.totalTemplate!=null && !totalTemplate.isEmpty()) {
-			sb.append(total.toString(totalTemplate));
-			sb.append(DELIMETER);
-		}
-		sb.append(footer);
-		sb.append(customFooter);
-		return sb.toString();
-	}
+    @Override
+    public void put(ArchiveDatabase2 db) throws IllegalStateException {
+        Calendar c = Calendar.getInstance();
+        // 1) find out period of the first week
+        ListIterator<ArchiveItem> it = null;
+        it = db.getIterator();
+        HashMap<String,Integer> userstat = new HashMap<String,Integer>(100);
+        //HashMap<Integer,Integer> articlestat = new HashMap<Integer,Integer>(7);
+        int[] articlestat = new int[366];
+        Arrays.fill(articlestat, 0);
+        
+        int total = 0;
+        int y = 0;
+        int day = 0;
+        ArchiveItem item = null;
+        while (it.hasNext()) {
+            item = it.next();
+            y = item.year;
+            if (y >= this.startYear) { // skip articles before the required year
+                userstat.put(item.user, 1);
+                c.set(item.year, item.month, item.day);
+                day = c.get(Calendar.DAY_OF_YEAR);
+                articlestat[day - 1] = 1;
+                total = 1;
+                break;
+            }
+        }
+
+        while (it.hasNext()) {
+            item = it.next();
+            if (item.year != y) {
+                // calculate stat and add to list 
+                this.addStat(y, total, articlestat, userstat);
+                y = item.year;
+                // zero stat
+                total = 0;
+                Arrays.fill(articlestat, 0);
+                userstat.clear();
+            } 
+            // add item to current stat
+            Integer n = userstat.get(item.user);
+            if (n == null) userstat.put(item.user, 1);
+            else userstat.put(item.user, n + 1);
+            c.set(item.year, item.month, item.day);
+            day = c.get(Calendar.DAY_OF_YEAR);
+            articlestat[day - 1]++;
+            total++;
+        }
+        // last stat
+        if (item != null) {
+            this.addStat(y, total, articlestat, userstat);
+        }
+
+        calcTotal();
+    }
+
+    void addStat(int year, int total, int[] articlestat, Map<String,Integer> userstat) {
+        log.debug("adding item: year={} total: {}", year, total);
+        StatItem stat = new StatItem();
+        stat.number = items.size() + 1;
+        stat.month = 0;
+        stat.year = year;
+        stat.articles = total;
+
+        int min = 1000000;
+        int max = 0;
+        int sum = 0;
+        Calendar c = Calendar.getInstance();
+        c.set(year, 1, 1);
+        int mdays = c.getActualMaximum(Calendar.DAY_OF_YEAR);
+        for (int i = 0; i < mdays; i++) {
+            int n = articlestat[i];
+            sum += n;
+            if (n < min) min = n;
+            if (n > max) max = n;
+        }
+        stat.articlesDayMid = ((float)sum / (float)mdays);
+        stat.articlesDayMax = max;
+        stat.articlesDayMin = min;
+        String user = "nobody";
+        int userc = -1;
+        for (Map.Entry<String, Integer> entry : userstat.entrySet()) {
+            if (entry.getValue() > userc) {
+                user = entry.getKey();
+                userc = entry.getValue();
+            }
+        }
+        stat.user = user;
+        stat.userArticles = userc;
+        if (!items.isEmpty()) {
+            StatItem prev = this.items.get(items.size() - 1);
+            stat.progress = stat.articles - prev.articles;
+        }
+        this.items.add(stat);
+        addUserStat(userstat);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(customHeader);
+        sb.append(header);
+        sb.append(DELIMETER);
+        for (StatItem item:items) {
+            String str = item.toString();
+            str = str.replace("%(отчет)", 
+                    this.reportTemplate.replace("%(год)", String.valueOf(item.year)));
+            sb.append(str);
+            sb.append(DELIMETER);
+        }        
+        if (this.totalTemplate != null && !totalTemplate.isEmpty()) {
+            sb.append(total.toString(totalTemplate));
+            sb.append(DELIMETER);
+        }
+        sb.append(footer);
+        sb.append(customFooter);
+        return sb.toString();
+    }
 
 }
